adbfa1edf0a513e33080d42e1416128650c2a57d
Suhan Shrestha <83650068+Sohann-jpg@users.noreply.github.com>
2024-12-18 11:28:35 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
 require("dotenv").config();
 const path = require("path");
+
+// Initialize Express App
+const app = express(); 
+
 const corsOptions = {
   origin: "https://trendfit.netlify.app", // Netlify frontend URL
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type"],
 };
-app.use(cors(corsOptions));
-
-// Initialize Express App
-const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions)); // No error now
 app.use(bodyParser.json());
 
 // Connect to MongoDB
@@ -25,7 +25,6 @@ connectDB();
 app.use("/api/products", require("./routes/productRoutes"));
 app.use("/uploads", express.static("uploads"));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
 
 // Start Server
 const PORT = process.env.PORT || 5000;
